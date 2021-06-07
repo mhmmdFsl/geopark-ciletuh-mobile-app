d1b0855a403beeced1c8dcfbc1e69dc639c29693
pandaEzz <muhammadfaisal160297@gmail.com>
2021-06-07 12:05:02 +0700

    menambahkan rekomendasi tempat wisata

--- src/Component/HomeScreen.js
+++ src/Component/HomeScreen.js
@@ -1,21 +1,48 @@
-import React from 'react'
-import { ScrollView, StyleSheet, Text, View } from 'react-native'
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'
+import React, {useState, useEffect} from 'react'
+import { ScrollView, StyleSheet, Text, View, FlatList } from 'react-native'
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps'
+import RecomenCard from './RecomenCard'
 
 const HomeScreen = () => {
+
+    const [dataSource, setDataSource] = useState({
+        title : 'Curug Cimarinjung',
+        uri: 'https://petualang.travelingyuk.com/uploads/2020/10/Curug-Cimarinjung-dengan-pemandangan-sawah-831x1024.jpg'
+    })
+
+    // useEffect(() => {
+    //     let items = Array.apply(null, Array(3)).map((v, i) => {
+    //         return {
+    //             id: i,
+    //             src: 'https://petualang.travelingyuk.com/uploads/2020/10/Curug-Cimarinjung-dengan-pemandangan-sawah-831x1024.jpg',
+    //             title: 'Curug Cimarinjung'
+    //         };
+    //     });
+    //     setDataSource(items);
+    // }, []);
+
     return(
             <View style={style.wrapper}>
+                <Text style = {style.title}>Lokasi Anda</Text>
                 <MapView
                     style={style.map}
                     provider={PROVIDER_GOOGLE}
                     initialRegion={{
-                        latitude: 37.78825,
-                        longitude: -122.4324,
-                        latitudeDelta: 0.0922,
-                        longitudeDelta: 0.0421,
-                    }}></MapView>
-                <ScrollView>
-                    <Text>Test</Text>
+                        latitude: -6.912303108754971,
+                        longitude: 106.89754117927,
+                        latitudeDelta: 0.0043,
+                        longitudeDelta: 0.0034,
+                    }}>
+                    <Marker
+                        coordinate={{ latitude: -6.912303108754971, longitude: 106.89754117927 }}
+                        image={{ uri: 'custom_pin' }}
+                    />
+                </MapView>
+                <Text style = {style. title}>Rekomendasi Wisata</Text>
+                <ScrollView style = {style.container} horizontal = {true}>
+                    <RecomenCard uri = {dataSource.uri} title = {dataSource.title} />
+                    <RecomenCard uri = {dataSource.uri} title = {dataSource.title} />
+                    <RecomenCard uri = {dataSource.uri} title = {dataSource.title} />
                 </ScrollView>
         </View>
     )
@@ -25,11 +52,20 @@ export {HomeScreen}
 
 const style = StyleSheet.create({
     wrapper: {
-        ...StyleSheet.absoluteFillObject
+        ...StyleSheet.absoluteFillObject,
+        paddingLeft: 15,
+        paddingRight: 15,
+        paddingTop: 15,
+        backgroundColor: '#fff'
+    },
+    container: {
     },
     map: {
         height: 170,
-        margin:15,
-        marginRight: 15
+        marginBottom: 15,
+    },
+    title: {
+        fontSize: 16,
+        marginBottom: 15
     }
 })
---
+++ src/Component/RecomenCard.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+
+function RecomenCard(props) {
+    return (
+        <View style = {style.wrapper}>
+            <Image source = {{uri: props.uri}} style = {style.img} />
+            <TouchableOpacity style = {style.titleLink}>
+                <Text style = {style.imgTitle}>{props.title}</Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+export default RecomenCard
+
+const style = StyleSheet.create({
+    wrapper: {
+        width: 150,
+        height: 200,
+        marginLeft: 15,
+        marginRight: 5,
+        borderRadius: 5,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.3,
+        shadowRadius: 5,
+
+        elevation: 7,
+    },
+    img: {
+        width: 150,
+        height: 200,
+        borderRadius: 5
+    },
+    titleLink: {
+        paddingTop: 10,
+        paddingLeft: 10,
+        height: 50,
+        width:150,
+        flex: 1,
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        backgroundColor: 'rgba(47, 47, 42, 0.5)'
+    },
+    imgTitle : {
+        color: '#fff',
+        fontSize: 15
+    }
+})
